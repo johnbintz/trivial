b60ffa8f5880bd33b122a5b3bb0f283f8ac5c42a
John Bintz <john@coswellproductions.com>
2010-03-21 13:17:04 -0400

    initial commit

---
+++ Rakefile
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('trivial', '0.0.1') do |p|
+	p.summary = "Ultra-lightweight website framework for PHP"
+	p.description = <<-EOT
+		For those who are using PHP to build their sites and want a very simple framework
+		in which to organize their files, trivial is the solution. It's one PHP file
+		that can include a few other pre-determined PHP and HTML files based on the
+		request URI. This very simple division of content, actions (controllers), and
+		views allows for multiple people to easily work on a smaller project without
+		the overhead of a larger framework.
+	EOT
+	p.author = "John Bintz"
+	p.email = "john@coswelproductions.com"
+	p.url = "http://github.com/johnbintz/trivial"
+end
---
+++ lib/trivial.php
@@ -0,0 +1,83 @@
+<?php
+
+// If you're using a different global default layout name, change it here
+$layout = 'application';
+
+// END OF USER-CONFIGURABLE SETTINGS
+
+function fe_check($path) {
+	global $root_dir;
+	if (file_exists($full_path = ($root_dir . '/' . $path))) {
+		return $full_path;
+	} else {
+		return false;
+	}
+}
+
+function partial($name, $local = array()) {
+	$name = preg_replace('#/([^/]+)$', '/_\1', $name);
+	if (($path = fe_check('views/' . $name . '.inc')) !== false) {
+		extract($local);
+		ob_start();
+		include($path);
+		return ob_get_clean();
+	} else {
+		trigger_error("No partial named ${name} found!");
+	}
+}
+
+$root_dir = realpath(dirname(__FILE__) . '/../');
+
+$trim = str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath($root_dir));
+
+$requested = preg_replace('#/$#', '/index.html', $_SERVER['REDIRECT_URL']);
+$requested = preg_replace("#${trim}/(.*)\.[^\.]+\$#", '\1', $requested);
+
+function styles($additional = array()) {
+	return head_component($additional, 'styles/%s.css',	'<link rel="stylesheet" href="%s" type="text/css" />');
+}
+
+function scripts($additional = array()) {
+	return head_component($additional, 'scripts/%s.js',	'<script type="text/javascript" src="%s"></script>');
+}
+
+function head_component($additional, $search, $format) {
+	global $requested;
+
+	$output = array();
+	foreach (array_merge(array('application', $requested), $additional) as $file) {
+		if (fe_check(sprintf($search, $file)) !== false) {
+			$output[] = sprintf($format, $file);
+		}
+	}
+	return implode("\n", $output);
+}
+
+$content = null;
+if (($content_file = fe_check('content/' . $requested . '.html')) !== false) {
+	$content = file_get_contents($content_file);
+}
+
+foreach (array('application', $requested) as $action) {
+	if (($action_file = fe_check('actions/' . $action . '.inc')) !== false) {
+		include($action_file);
+	}
+}
+
+if (($view_file = fe_check('views/' . $requested . '.inc')) !== false) {
+	ob_start();
+	include($view_file);
+	$content = ob_get_clean();
+}
+
+if (is_null($content)) {
+	trigger_error("No content generated for ${requested}! Did you create a content, action, or view file for this request?");
+}
+
+if (($layout_file = fe_check('views/' . $layout . '.inc')) !== false) {
+	ob_start();
+	include($layout_file);
+	$content = ob_get_clean();
+}
+
+echo $content;
